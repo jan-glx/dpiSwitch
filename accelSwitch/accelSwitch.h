948f36f93b623d30a4b56ce0e6b09e5c4d05b4ef
Jan Gleixner <jan.gleixner@gmail.com>
2016-04-15 16:48:56 +0200

    switched to window process - works :))
    implemented #1

--- accelSwitch/accelSwitch.h
+++ accelSwitch/accelSwitch.h
@@ -1,4 +1,19 @@
 
 #pragma once
+#include <string>
 
 int switchMouseAcceleration(bool turnOn, bool toggle = false);
+
+
+// convert UTF-8 string to wstring
+std::wstring utf8_to_wstring(const std::string& str);
+
+// convert wstring to UTF-8 string
+std::string wstring_to_utf8(const std::wstring& str);
+
+void printError(std::string sFailingFunction);
+
+
+// Format a readable error message, 
+// and exit from the application.
+void ErrorExit(std::string lpszFunction);
