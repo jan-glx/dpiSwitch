5b3dfd4deb2e187a2eec9ed3c4f1a11f4e8cbec4
Jan Gleixner <jan.gleixner@gmail.com>
2023-06-28 15:26:16 +0200

    switch to cpp 20 and use static libs

--- dpiSwitcher/dpiSwitcher.cpp
+++ dpiSwitcher/dpiSwitcher.cpp
@@ -4,10 +4,12 @@
 #include "stdafx.h"
 #include "dpiSwitcher.h"
 #include "dpiSwitch.h"
+#include "DpiHelper.h"
 #include <dbt.h>
 #include <string>
 #include <codecvt>
 #include <iostream>
+#include <vector>
 
 #define MAX_LOADSTRING 100
 
@@ -200,4 +202,63 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 		return DefWindowProc(hWnd, message, wParam, lParam);
 	}
 	return 0;
+}
+
+//to store display info along with corresponding list item
+struct DisplayData {
+	LUID m_adapterId;
+	int m_targetID;
+	int m_sourceID;
+
+	DisplayData()
+	{
+		m_adapterId = {};
+		m_targetID = m_sourceID = -1;
+	}
+};
+
+std::vector<DisplayData> GetDisplayData()
+{
+	std::vector<DisplayData> displayDataCache;
+	std::vector<DISPLAYCONFIG_PATH_INFO> pathsV;
+	std::vector<DISPLAYCONFIG_MODE_INFO> modesV;
+	int flags = QDC_ONLY_ACTIVE_PATHS;
+	if (false == DpiHelper::GetPathsAndModes(pathsV, modesV, flags))
+	{
+		std::cout << "DpiHelper::GetPathsAndModes() failed\n";
+	}
+	displayDataCache.resize(pathsV.size());
+	for (int idx = 0; const auto & path : pathsV)
+	{
+		//get display name
+		auto adapterLUID = path.targetInfo.adapterId;
+		auto targetID = path.targetInfo.id;
+		auto sourceID = path.sourceInfo.id;
+
+		DISPLAYCONFIG_TARGET_DEVICE_NAME deviceName;
+		deviceName.header.size = sizeof(deviceName);
+		deviceName.header.type = DISPLAYCONFIG_DEVICE_INFO_GET_TARGET_NAME;
+		deviceName.header.adapterId = adapterLUID;
+		deviceName.header.id = targetID;
+		if (ERROR_SUCCESS != DisplayConfigGetDeviceInfo(&deviceName.header))
+		{
+			std::cout << "DisplayConfigGetDeviceInfo() failed";
+		}
+		else
+		{
+			std::wstring nameString = std::to_wstring(idx) + std::wstring(L". ") + deviceName.monitorFriendlyDeviceName;
+			if (DISPLAYCONFIG_OUTPUT_TECHNOLOGY_INTERNAL == deviceName.outputTechnology)
+			{
+				nameString += L"(internal display)";
+			}
+			DisplayData dd = {};
+			dd.m_adapterId = adapterLUID;
+			dd.m_sourceID = sourceID;
+			dd.m_targetID = targetID;
+
+			displayDataCache[idx] = dd;
+		}
+		idx += 1;
+	}
+	return displayDataCache;
 }
